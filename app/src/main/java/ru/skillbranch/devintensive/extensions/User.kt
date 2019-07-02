1275c5030fc4c29f45b694251d5479bb18f187ad
Khadgar <blyashuk@list.ru>
2019-07-02 15:01:36 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -1,9 +1,8 @@
-package ru.skillbranch.devintensive.extension
+package ru.skillbranch.devintensive.extensions
 
 import ru.skillbranch.devintensive.models.User
 import ru.skillbranch.devintensive.models.UserView
 import ru.skillbranch.devintensive.utils.Utils
-import java.util.*
 
 fun User.toUserView() : UserView {
 
