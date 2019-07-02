142eaa31259cf3eb301e3c1811eec189d50de914
Khadgar <blyashuk@list.ru>
2019-07-02 16:10:42 +0300

    hometask_2 changes completed

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -11,50 +11,85 @@ object Utils {
         return Pair(parts?.getOrNull(0), parts?.getOrNull(1))
     }
 
-    fun transliteration(payLoad: String, divider: String = " "): String {
+    fun transliteration(payload: String, divider: String = " "): String {
+        var result = String()
 
-        val nickNameLowerCase = payLoad.toLowerCase()
+        for (symbol in payload){
+            if(symbol == ' ') result += divider
+            if(translitMap.containsKey(symbol))
+                result += translitMap[symbol]
+        }
 
-        return nickNameLowerCase.replace(Regex("[абвгдеёжзийклмнопрстуфхцчшщъыьэюя ]")) {
-              when (it.value) {
-                  "а" -> "a"
-                  "б" -> "b"
-                  "в" -> "v"
-                  "г" -> "g"
-                  "д" -> "d"
-                  "е" -> "e"
-                  "ё" -> "e"
-                  "ж" -> "zh"
-                  "з" -> "z"
-                  "и" -> "i"
-                  "й" -> "i"
-                  "к" -> "k"
-                  "л" -> "l"
-                  "м" -> "m"
-                  "н" -> "n"
-                  "о" -> "o"
-                  "п" -> "p"
-                  "р" -> "r"
-                  "с" -> "s"
-                  "т" -> "t"
-                  "у" -> "u"
-                  "ф" -> "f"
-                  "х" -> "h"
-                  "ц" -> "c"
-                  "ч" -> "ch"
-                  "ш" -> "sh"
-                  "щ" -> "sh'"
-                  "ъ" -> ""
-                  "ы" -> "i"
-                  "ь" -> ""
-                  "э" -> "e"
-                  "ю" -> "yu"
-                  "я" -> "ya"
-                  " " -> "_"
-                  else -> it.value
-              }
-          }
+        return result
     }
+
+    private val translitMap = mutableMapOf(
+        'а' to "a",
+        'б' to "b",
+        'в' to "v",
+        'г' to "g",
+        'д' to "d",
+        'е' to "e",
+        'ё' to "e",
+        'ж' to "zh",
+        'з' to "z",
+        'и' to "i",
+        'й' to "i",
+        'к' to "k",
+        'л' to "l",
+        'м' to "m",
+        'н' to "n",
+        'о' to "o",
+        'п' to "p",
+        'р' to "r",
+        'с' to "s",
+        'т' to "t",
+        'у' to "u",
+        'ф' to "f",
+        'х' to "h",
+        'ц' to "c",
+        'ч' to "ch",
+        'ш' to "sh",
+        'щ' to "sh",
+        'ъ' to "",
+        'ь' to "",
+        'э' to "e",
+        'ю' to "yu",
+        'я' to "ya",
+
+        'А' to "A",
+        'Б' to "B",
+        'В' to "V",
+        'Г' to "G",
+        'Д' to "D",
+        'Е' to "E",
+        'Ё' to "E",
+        'Ж' to "Zh",
+        'З' to "Z",
+        'И' to "I",
+        'Й' to "I",
+        'К' to "K",
+        'Л' to "L",
+        'М' to "M",
+        'Н' to "N",
+        'О' to "O",
+        'П' to "P",
+        'Р' to "R",
+        'С' to "S",
+        'Т' to "T",
+        'У' to "U",
+        'Ф' to "F",
+        'Х' to "H",
+        'Ц' to "C",
+        'Ч' to "Ch",
+        'Ш' to "Sh",
+        'Щ' to "Sh",
+        'Ъ' to "",
+        'Ь' to "",
+        'Э' to "E",
+        'Ю' to "Yu",
+        'Я' to "Ya"
+    )
 
     fun toInitials(firstName: String?, lastName: String?): String? {
         if (firstName.isNullOrBlank() && lastName.isNullOrBlank())
