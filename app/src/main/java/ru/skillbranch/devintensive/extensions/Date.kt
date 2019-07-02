29007a3439b2ef09c65e20823f7afaea7d381e96
Khadgar <blyashuk@list.ru>
2019-07-02 18:57:49 +0300

    hometask_2 changes completed

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,11 +1,7 @@
 package ru.skillbranch.devintensive.extensions
 
-
-import ru.skillbranch.devintensive.utils.Utils
-import java.lang.StrictMath.abs
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.math.roundToInt
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
@@ -17,15 +13,6 @@ fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     return dateFormat.format(this)
 }
 
-fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
-    this.time += when (units) {
-        TimeUnits.SECOND -> value * SECOND
-        TimeUnits.MINUTE -> value * MINUTE
-        TimeUnits.HOUR -> value * HOUR
-        TimeUnits.DAY -> value * DAY
-    }
-    return this
-}
 
 enum class TimeUnits {
     SECOND,
@@ -34,88 +21,58 @@ enum class TimeUnits {
     DAY,
 }
 
+
+fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
+    var time = time
+    time += when (units) {
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
+    }
+    this.time = time
+    return this
+}
+
 fun Date.humanizeDiff(date: Date = Date()): String {
-    val diff = this.time - date.time
-
-    return if (diff >= 0) {
-        humanizeFuture(abs(diff))
-    } else {
-        humanizePast(abs(diff))
+    val diff = date.time - time
+    val correctForm: String
+    val minForms = listOf("минута", "минуты", "минут")
+    val hourForms = listOf("час", "часа", "часов")
+    val dayForms = listOf("день", "дня", "дней")
+    val pluralFinder: (forms: List<String>, quantity: Long, unit: Long) -> String = { list, quantity, unit ->
+        if (quantity / unit in 11L..14L) {
+            list[2]
+        } else {
+            when ((quantity / unit) % 10) {
+                1L -> list[0]
+                2L, 3L, 4L -> list[1]
+                in 5L..9L -> list[2]
+                0L -> list[2]
+                else -> throw IllegalStateException("wrong time")
+            }
+        }
     }
-}
-
-private fun humanizeFuture(diff: Long): String {
-    /**
-    0с - 1с "только что"
-    1с - 45с "через несколько секунд"
-    45с - 75с "через минуту"
-    75с - 45мин "через N минуту;минуты;минут"
-    45мин - 75мин "через час"
-    75мин 22ч "через N час;часа;часов"
-    22ч - 26ч "через день"
-    26ч - 360д "через N день;дня;дней"
-    >360д "более чем через год"
-     */
-
     return when (diff) {
-        in 0..(1 * SECOND) -> "только что"
-        in (1 * SECOND + 1)..(45 * SECOND) -> "через несколько секунд"
-        in (45 * SECOND + 1)..(75 * SECOND) -> "через минуту"
-        in (75 * SECOND + 1)..(45 * MINUTE) -> {
-            val amount = calcDiffAmount(diff, MINUTE)
-            "через $amount ${Utils.getPluralForm("минуту;минуты;минут", amount)}"
+        in 0L..SECOND -> "только что"
+        in SECOND..SECOND * 45 -> "несколько секунд назад"
+        in SECOND * 45..SECOND * 75 -> "минуту назад"
+        in SECOND * 75..MINUTE * 45 -> {
+            correctForm = pluralFinder(minForms, diff, MINUTE)
+            "${diff / MINUTE} $correctForm назад"
         }
-        in (45 * MINUTE + 1)..(75 * MINUTE) -> "через час"
-        in (75 * MINUTE + 1)..(22 * HOUR) -> {
-            val amount = calcDiffAmount(diff, HOUR)
-            "через $amount ${Utils.getPluralForm("час;часа;часов", amount)}"
+        in MINUTE * 45..MINUTE * 75 -> "час назад"
+        in MINUTE * 75..HOUR * 22 -> {
+            correctForm = pluralFinder(hourForms, diff, HOUR)
+            "${diff / HOUR} $correctForm назад"
         }
-        in (22 * HOUR + 1)..(26 * HOUR) -> "через день"
-        in (26 * HOUR + 1)..(360 * DAY) -> {
-            val amount = calcDiffAmount(diff, DAY)
-            "через $amount ${Utils.getPluralForm("день;дня;дней", amount)}"
-        }
-        else -> "более чем через год"
-    }
-}
-
-private fun humanizePast(diff: Long): String {
-    /**
-    0с - 1с "только что"
-    1с - 45с "несколько секунд назад"
-    45с - 75с "минуту назад"
-    75с - 45мин "N минуту;минуты;минут назад"
-    45мин - 75мин "час назад"
-    75мин 22ч "N час;часа;часов назад"
-    22ч - 26ч "день назад"
-    26ч - 360д "N день;дня;дней назад"
-    >360д "более года назад"
-     */
-
-    return when (diff) {
-        in 0..(1 * SECOND) -> "только что"
-        in (1 * SECOND + 1)..(45 * SECOND) -> "несколько секунд назад"
-        in (45 * SECOND + 1)..(75 * SECOND) -> "минуту назад"
-        in (75 * SECOND + 1)..(45 * MINUTE) -> {
-            val amount = calcDiffAmount(diff, MINUTE)
-            "$amount ${Utils.getPluralForm("минуту;минуты;минут", amount)} назад"
-        }
-        in (45 * MINUTE + 1)..(75 * MINUTE) -> "час назад"
-        in (75 * MINUTE + 1)..(22 * HOUR) -> {
-            val amount = calcDiffAmount(diff, HOUR)
-            "$amount ${Utils.getPluralForm("час;часа;часов", amount)} назад"
-        }
-        in (22 * HOUR + 1)..(26 * HOUR) -> "день назад"
-        in (26 * HOUR + 1)..(360 * DAY) -> {
-            val amount = calcDiffAmount(diff, DAY)
-            "$amount ${Utils.getPluralForm("день;дня;дней", amount)} назад"
+        in HOUR * 22..HOUR * 26 -> "день назад"
+        in HOUR * 26..DAY * 360 -> {
+            correctForm = pluralFinder(dayForms, diff, DAY)
+            "${diff / DAY} $correctForm назад"
         }
         else -> "более года назад"
     }
 }
 
-private fun calcDiffAmount(diff: Long, timeInterval: Long): Int {
-    val result: Float = (diff.toFloat() / timeInterval.toFloat())
-    return result.roundToInt()
-}
 
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -105,16 +105,4 @@ object Utils {
         }).toUpperCase()
     }
 
-    fun getPluralForm(pluralForms: String, count: Int): String {
-        val forms = pluralForms.split(";")
-        when (count % 10) {
-            1 -> if (count % 100 != 11)
-                return forms[0]
-            in 2..4 -> if (count % 100 !in 12..14) {
-                return forms[1]
-            }
-        }
-
-        return forms[2]
-    }
     }
