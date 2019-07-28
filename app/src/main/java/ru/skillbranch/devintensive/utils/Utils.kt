d0d5b78e873b9a418bf522b1cd4c855fcf7bb7e9
Khadgar <blyashuk@list.ru>
2019-07-28 23:10:45 +0300

    hometask_4 03.07 2 commit - changes

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.utils
 
+import android.content.Context
+
 object Utils {
 
     fun parseFullName(fullName: String?): Pair<String?, String?> {
@@ -103,6 +105,21 @@ object Utils {
             lNameInitial == null -> "$fNameInitial"
             else -> "$fNameInitial$lNameInitial"
         }).toUpperCase()
+    }
+
+
+    fun convertPxToDp(context: Context, px: Int): Int {
+        val scale = context.resources.displayMetrics.density
+        return (px / scale + 0.5f).toInt()
+    }
+
+    fun convertDpToPx(context: Context, dp: Int): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
+    }
+
+    fun convertSpToPx(context: Context, sp: Int): Int {
+        return sp * context.resources.displayMetrics.scaledDensity.toInt()
     }
 
     }
